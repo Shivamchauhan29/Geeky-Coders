1f045f84f3568fd2987f70a10c745b17708b452d
Shivam Chauhan <81063248+Shivamchauhan29@users.noreply.github.com>
2022-10-03 12:53:50 +0530

    Create Roll Dice .py

---
+++ Roll Dice .py
@@ -0,0 +1,49 @@
+while True:
+    import random
+    print('this is a program to calculate a dice')
+    print('press enter ')
+    input()
+    n=random.randint(1,6)
+    if n==1:
+          print('[====================]')
+          print('[                    ]')
+          print('[          o         ]')
+          print('[                    ]')
+          print('[====================]')
+          
+    if(n==2):
+         
+         
+         print('[====================]')
+         print('[                    ]')
+         print('[   o     2     o    ]')
+         print('[                    ]')
+         print('[====================]')
+         
+    if(n==3):
+          print('[====================]')
+          print('[    o          o    ]')
+          print('[         o          ]')
+          print('[         3          ]')
+          print('[====================]')
+          
+    if(n==4):
+          print('[====================]')
+          print('[    o          o    ]')
+          print('[          4         ]')
+          print('[    o          o    ]')
+          print('[====================]')
+          
+    if(n==5):
+          print('[====================]')
+          print('[    o          o    ]')
+          print('[         o          ]')
+          print('[    o      5    o   ]')
+          print('[====================]')
+          
+    if(n==6):
+          print('[====================]')
+          print('[    o          o    ]')
+          print('[    o    6     o    ]')
+          print('[    o          o    ]')
+          print('[====================]')
